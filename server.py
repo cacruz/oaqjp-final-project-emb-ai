781da31d2e875692ccef39678202c3ddf77898c7
Carlos Cruz <carlos.a.cruz@gmail.com>
2026-01-18 10:52:27 -0500

    Add docstrings

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+"""
+server.py
+
+This module implements a Flask web application that exposes an emotion
+detection service using a pre-trained NLP emotion model. The application
+provides a web interface and an HTTP endpoint for analyzing user-submitted
+text and identifying the dominant emotion expressed in the text.
+"""
+
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +15,15 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Analyzes user-provided text and determines the dominant emotion expressed.
+
+    Returns
+    -------
+    str
+        A formatted message describing the dominant emotion and its score,
+        or an error message if the input text is invalid.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -28,6 +47,4 @@ def render_index_page():
 
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
