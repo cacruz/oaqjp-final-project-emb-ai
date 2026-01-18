536a54bff329eef1392ebe0167efb60c3e202bca
Carlos Cruz <carlos.a.cruz@gmail.com>
2026-01-18 10:29:55 -0500

    Add server code

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    label = response.get('dominant_emotion')
+
+    if label is None:
+        return "Invalid input! Try again."
+
+    score = response[label]
+
+    return (
+        f"The given text has been identified as {label} "
+        f"with a score of {score}."
+    )
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    ''' This functions executes the flask app and deploys it on localhost:5000
+    '''
+    app.run(host="0.0.0.0", port=5000)
